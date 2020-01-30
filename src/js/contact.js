a06fcd9b6fcbd9789c40fc16166d1720f93b786e
majovainilla <maria_reyes@me.com>
2020-01-29 21:41:56 -0600

    Edit contact section styling

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,16 +1,25 @@
 function contact() {
-  const section = document.querySelector('.landing-section');
+  const section = document.querySelector('.landing-section', 'row');
   const title = document.createElement('h1');
   const text = document.createElement('p');
+  const div = document.createElement('div');
+  const img = document.createElement('div');
 
   const renderContact = () => {
     section.classList.add('contact');
-    title.classList.add('main-title', 'text-white');
-    text.classList.add('text-white', 'text-center');
-    title.textContent = 'Contact Us';
-    text.textContent = 'We deliver an amazing experience without any harm';
-    section.append(title);
-    section.append(text);
+    title.classList.add('contact-title');
+    text.classList.add('contact-text');
+    img.classList.add('contact-img');
+    div.classList.add('text-container', 'col-6', 'pl-5');
+    title.textContent = 'Reservations';
+    text.innerHTML += `Make your day special by dinning with us in a great atmosphere.<br><br>
+                        If you can imagine it, we can host your event. Our specialty are large parties.<br><br>
+                        Phone: +52 1 22 55 22 55 <br>
+                        Address: rue de Montparnasse 345`;
+    div.append(title);
+    div.append(text);
+    section.append(img);
+    section.append(div);
   };
 
   renderContact();
--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,7 @@
 function home() {
   const section = document.querySelector('.landing-section');
   const title = document.createElement('h1');
-  const text = `<p class="text-white text-center text"> We deliver an amazing experience without harming animals.
+  const text = `<p class="text-white text"> We deliver an amazing experience without harming animals.
                 <br>
                 We are commited to health, but also to keeping other lives.
                 <br>
@@ -11,9 +11,9 @@ function home() {
 
   const renderHome = () => {
     section.classList.add('cover', 'justify-content-end');
-    title.classList.add('main-title', 'pb-5');
+    title.classList.add('main-title', 'col-12');
     title.textContent = 'Conscious Cuisine';
-    div.classList.add('col-4');
+    div.classList.add('col-5', 'pl-5', 'pr-0', 'row', 'align-items-center');
     div.append(title);
     div.innerHTML += text;
     section.append(div);
